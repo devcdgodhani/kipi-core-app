e2222651e855f14f63636cdde8fe9ca8b2a05448
dev.cdgodhani <dev.cdgodhani@gmail.com>
2026-02-12 18:24:31 +0530

    config: Add Vercel rewrite for SPA routing and update API base URL to production endpoint.

--- mobile/src/services/http.ts
+++ mobile/src/services/http.ts
@@ -2,7 +2,7 @@ import axios, { type AxiosInstance, type AxiosResponse } from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL:'http://10.10.10.168:3000/api/v1/customer', // Update this for production
+  baseURL:'https://mamavastra-api.ninewardrobe.com/api/v1/customer', // Update this for production
   headers: {
     'Content-Type': 'application/json',
   },
